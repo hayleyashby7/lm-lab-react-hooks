9e04b9d9577414fcafd917bf90d4395d77affcc8
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-23 13:08:52 +0100

    feat: :art: Use_memo exercise

--- src/components/hooks/use_memo/useMemo.tsx
+++ src/components/hooks/use_memo/useMemo.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export const MemoExample = () => {
-
-	console.log("Rendering component...")
+	console.log('Rendering component...');
 
 	const [numberObj, setNumberObj] = useState({ input: 0 });
-	
-	const doubleNumber = slowFunction(numberObj.input)
+
+	const doubleNumber = useMemo(() => slowFunction(numberObj.input), [numberObj.input]);
 
 	return (
 		<>
 			<h2>useMemo</h2>
 
 			<button onClick={() => setNumberObj({ input: 3 })}>Double 3</button>
+			<button onClick={() => setNumberObj({ input: 4 })}>Double 4</button>
+			<button onClick={() => setNumberObj({ input: 5 })}>Double 5</button>
 
 			<p className='use-memo__text'>{doubleNumber}</p>
 		</>
